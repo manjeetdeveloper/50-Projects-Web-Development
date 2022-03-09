ba40f7d9633b6606b2047bcd112f01dd70126a4f
Manjeet <manjeetkyp24@gmail.com>
2022-03-09 14:07:37 +0530

    new changes

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const boxes = document.querySelectorAll('.box')
 
-let load = 0
+window.addEventListener('scroll', checkBoxes)
 
-let int = setInterval(blurring, 30)
+checkBoxes()
 
-function blurring() {
-  load++
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
 
-  if (load > 99) {
-    clearInterval(int)
-  }
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
 
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
-
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
-
